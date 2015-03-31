8bc34cdd27c331d27e46b118daba4fd07dbeb5ea
Joao Vicente <vicentejo@dnb.com>
2015-03-31 22:18:46 +0100

    added test with logic for tested basic POST request/response

--- src/test/java/com/apm4all/tracy/TracyPublisherTest.java
+++ src/test/java/com/apm4all/tracy/TracyPublisherTest.java
@@ -1,27 +1,79 @@
 package com.apm4all.tracy;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TracyPublisherTest {
 	TracyPublisher pub;
+	Tomcat tomcat;
 	
 	@Before
 	public void setUp() throws Exception {
 		pub = new TracyPublisher();
+		// Start Tomcat
+		tomcat = new Tomcat();
+		tomcat.setPort(8080);
+		File base = new File(System.getProperty("java.io.tmpdir"));
+		Context rootCtx = tomcat.addContext("/tracy", base.getAbsolutePath());
+		Tomcat.addServlet(rootCtx, "segment", new TracySegmentServlet());
+		rootCtx.addServletMapping("/segment", "segment");
+		tomcat.start();
+//		tomcat.getServer().await();
 	}
 
+	private String post(String tracySegment) throws ClientProtocolException, IOException	{
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpPost httpPost = new HttpPost("http://localhost:8080/tracy/segment");
+		StringEntity se=new StringEntity(tracySegment,"UTF-8");
+		se.setContentType("application/json");
+		httpPost.setEntity(se);
+		httpPost.setHeader("Content-Type","application/json;charset=UTF-8");
+		String responseString;
+		
+		CloseableHttpResponse response = httpclient.execute(httpPost);
+		
+		try {
+		    System.out.println(response.getStatusLine());
+		    HttpEntity entity = response.getEntity();
+		    // do something useful with the response body
+		    // and ensure it is fully consumed
+		    responseString = EntityUtils.toString(entity, "UTF-8");
+		    System.out.println(responseString);
+		    EntityUtils.consume(entity);
+		} finally {
+		    response.close();
+		}
+		return(responseString);
+	}
+	
 	@Test
-	public void test() {
+	public void test() throws LifecycleException, ClientProtocolException, IOException {
 		Tracy.setContext("MyTask", "null", "MyComponent");
 		Tracy.before("myLabel1");
 		Tracy.before("myLabel1");
 		// FIXME: replace with Tracy.getEventsAsJson()
 		String tracySegment = Tracy.getEventsAsJson().get(0);
-		pub.publish(tracySegment);
-		fail("Not yet implemented");
+		String postResponse = this.post(tracySegment);
+		
+//		pub.publish(tracySegment);
+		assertEquals(tracySegment, postResponse);
 	}
 
 }
---
+++ src/test/java/com/apm4all/tracy/TracySegmentServlet.java
@@ -0,0 +1,62 @@
+package com.apm4all.tracy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TracySegmentServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	 
+	@Override
+	protected void doGet(final HttpServletRequest req,
+			final HttpServletResponse res) throws ServletException, IOException {
+		res.getWriter()
+				.append(String.format("It's %s now\n\n\n\nwww.hascode.com",
+						new Date()));
+	}
+	
+	@Override
+	protected void doPost(final HttpServletRequest req,
+			final HttpServletResponse res) throws ServletException, IOException {
+		
+	    String body = null;
+	    StringBuilder stringBuilder = new StringBuilder();
+	    BufferedReader bufferedReader = null;
+
+	    try {
+	        InputStream inputStream = req.getInputStream();
+	        if (inputStream != null) {
+	            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+	            char[] charBuffer = new char[128];
+	            int bytesRead = -1;
+	            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+	                stringBuilder.append(charBuffer, 0, bytesRead);
+	            }
+	        } else {
+	            stringBuilder.append("");
+	        }
+	    } catch (IOException ex) {
+	        throw ex;
+	    } finally {
+	        if (bufferedReader != null) {
+	            try {
+	                bufferedReader.close();
+	            } catch (IOException ex) {
+	                throw ex;
+	            }
+	        }
+	    }
+
+	    body = stringBuilder.toString();
+		
+//		res.getWriter().append(String.format("It's %s now\n\n\n\nwww.hascode.com", new Date()));
+		res.getWriter().append(body);
+	}	
+}
